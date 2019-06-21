6cb58349ceb211bb17083f03f8915a66f3ef51e7
daief <1437931235@qq.com>
2019-06-21 09:51:31 +0800

    refactor: add detail query loading

--- src/options/SetDetail/index.tsx
+++ src/options/SetDetail/index.tsx
@@ -20,6 +20,7 @@ import {
   List,
   Row,
   Select,
+  Skeleton,
   Spin,
   Tooltip,
 } from 'antd';
@@ -36,6 +37,7 @@ export const mapState = () =>
   useCallback<
     AnyFunc<{
       saveLoading: boolean;
+      detailLoading: boolean;
       detail: FileSetDetail | undefined;
       detailCopy: FileSetDetail | undefined;
       globalOptions: ExtensionGlobalOptions;
@@ -43,6 +45,7 @@ export const mapState = () =>
   >(
     _ => ({
       saveLoading: _.loading.effects.options.saveFileSet,
+      detailLoading: _.loading.effects.options.getFileSetDetail,
       detail: _.options.detail,
       detailCopy: _.options.detailCopy,
       globalOptions: _.all.globalOptions,
@@ -52,7 +55,9 @@ export const mapState = () =>
 
 export const SetDetail: React.SFC = props => {
   const { dispatch } = useStore();
-  const { saveLoading, detail, detailCopy } = useMappedState(mapState());
+  const { saveLoading, detailLoading, detail, detailCopy } = useMappedState(
+    mapState(),
+  );
   const setDetail = (_: Partial<FileSetDetail> = {}) =>
     dispatch.options.setState({
       detail: {
@@ -151,100 +156,104 @@ export const SetDetail: React.SFC = props => {
     }
   };
 
-  return detail ? (
-    <Spin spinning={saveLoading}>
-      <Row gutter={16}>
-        <Col md={12} sm={24}>
-          <Form.Item label="Name" validateStatus={name ? '' : 'error'}>
-            <Input value={name} onChange={handleNameChange} />
-          </Form.Item>
-        </Col>
-        <Col md={12} sm={24}>
-          <TopActions />
-        </Col>
-      </Row>
-      <List
-        dataSource={ruleList.length ? ruleList : ['add', ...ruleList]}
-        rowKey="id"
-        grid={{ gutter: 8, md: 2, sm: 1 }}
-        bordered
-        header={
-          <>
-            <Tooltip
-              placement="topLeft"
-              title="Matching rules follow JS RegExp. Matching type affects the matching range of a url."
-            >
-              <Icon type="question-circle" />
-            </Tooltip>{' '}
-            Rule list
-          </>
-        }
-        size="small"
-        renderItem={(rule: Rule) => {
-          if (typeof rule === 'string') {
-            return (
-              <List.Item style={{ marginTop: 8 }}>
-                <Button
-                  icon="plus"
-                  type="dashed"
-                  style={{
-                    width: '100%',
-                  }}
-                  size="small"
-                  onClick={handleAddNewRuleOfSet}
-                />
-              </List.Item>
-            );
-          }
-          const {
-            id: ruleId,
-            regexContent,
-            matchType,
-            status: ruleStatus,
-          } = rule;
-          return (
-            <List.Item style={{ marginTop: 8 }}>
-              <Row gutter={8}>
-                <Col md={14} sm={24}>
-                  <Input
-                    value={regexContent}
-                    onChange={handleRuleContentChange(ruleId)}
-                    placeholder="Write your regex here"
-                    size="small"
-                    disabled={ruleStatus === STATUS.DISABLE}
-                  />
-                </Col>
-                <Col md={6} sm={16} xs={16}>
-                  <Select
-                    size="small"
-                    value={matchType}
-                    onChange={handleRuleMatchTypeChange(ruleId)}
-                    style={{ width: '100%' }}
-                  >
-                    {renderOptions(MATCH_TYPE_OPTIONS())}
-                  </Select>
-                </Col>
-                <Col md={4} sm={8} xs={8}>
-                  <ToggleStatusButton
-                    size="small"
-                    value={ruleStatus}
-                    onChange={handleRuleToggleStatusClick(ruleId)}
-                  />
-                  <Button
-                    type="danger"
-                    onClick={handleClickDeleteRule(ruleId)}
-                    size="small"
-                    icon="delete"
-                  />
-                </Col>
-              </Row>
-            </List.Item>
-          );
-        }}
-      />
-      <CodeList />
-    </Spin>
-  ) : (
-    <Empty />
+  return (
+    <Skeleton loading={detailLoading} paragraph={{ rows: 6 }}>
+      {detail ? (
+        <Spin spinning={saveLoading}>
+          <Row gutter={16}>
+            <Col md={12} sm={24}>
+              <Form.Item label="Name" validateStatus={name ? '' : 'error'}>
+                <Input value={name} onChange={handleNameChange} />
+              </Form.Item>
+            </Col>
+            <Col md={12} sm={24}>
+              <TopActions />
+            </Col>
+          </Row>
+          <List
+            dataSource={ruleList.length ? ruleList : ['add', ...ruleList]}
+            rowKey="id"
+            grid={{ gutter: 8, md: 2, sm: 1 }}
+            bordered
+            header={
+              <>
+                <Tooltip
+                  placement="topLeft"
+                  title="Matching rules follow JS RegExp. Matching type affects the matching range of a url."
+                >
+                  <Icon type="question-circle" />
+                </Tooltip>{' '}
+                Rule list
+              </>
+            }
+            size="small"
+            renderItem={(rule: Rule) => {
+              if (typeof rule === 'string') {
+                return (
+                  <List.Item style={{ marginTop: 8 }}>
+                    <Button
+                      icon="plus"
+                      type="dashed"
+                      style={{
+                        width: '100%',
+                      }}
+                      size="small"
+                      onClick={handleAddNewRuleOfSet}
+                    />
+                  </List.Item>
+                );
+              }
+              const {
+                id: ruleId,
+                regexContent,
+                matchType,
+                status: ruleStatus,
+              } = rule;
+              return (
+                <List.Item style={{ marginTop: 8 }}>
+                  <Row gutter={8}>
+                    <Col md={14} sm={24}>
+                      <Input
+                        value={regexContent}
+                        onChange={handleRuleContentChange(ruleId)}
+                        placeholder="Write your regex here"
+                        size="small"
+                        disabled={ruleStatus === STATUS.DISABLE}
+                      />
+                    </Col>
+                    <Col md={6} sm={16} xs={16}>
+                      <Select
+                        size="small"
+                        value={matchType}
+                        onChange={handleRuleMatchTypeChange(ruleId)}
+                        style={{ width: '100%' }}
+                      >
+                        {renderOptions(MATCH_TYPE_OPTIONS())}
+                      </Select>
+                    </Col>
+                    <Col md={4} sm={8} xs={8}>
+                      <ToggleStatusButton
+                        size="small"
+                        value={ruleStatus}
+                        onChange={handleRuleToggleStatusClick(ruleId)}
+                      />
+                      <Button
+                        type="danger"
+                        onClick={handleClickDeleteRule(ruleId)}
+                        size="small"
+                        icon="delete"
+                      />
+                    </Col>
+                  </Row>
+                </List.Item>
+              );
+            }}
+          />
+          <CodeList />
+        </Spin>
+      ) : (
+        <Empty />
+      )}
+    </Skeleton>
   );
 };
